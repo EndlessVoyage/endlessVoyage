a4df6bcbb4dd80a7ed647041d2c9d77605b090fd
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-10-27 21:00:52 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -8,15 +8,15 @@ mkdir -p dist/linux
 mkdir -p dist/windows
 
 echo 'Building for web ...'
-godot --no-window --export "HTML5" dist/web/index.html
+godot --no-window --export-release "HTML5" dist/web/index.html
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --no-window --export 'Linux/X11' dist/linux/rootedJourney.x86_64
+godot --no-window --export-release 'Linux/X11' dist/linux/rootedJourney.x86_64
 chmod a+x dist/linux/rootedJourney.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --no-window --export 'Windows Desktop' dist/windows/rootedJourney.exe
+godot --no-window --export-release 'Windows Desktop' dist/windows/rootedJourney.exe
 echo 'done.'
 
