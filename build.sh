aa2dd3a23509b6fc79a7499590bbbc7ca0037e4f
Thomas Christlieb <thomaschristlieb@hotmail.com>
2023-10-27 20:52:01 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+rm -rf dist/
+mkdir -p dist/web
+mkdir -p dist/linux
+mkdir -p dist/windows
+
+echo 'Building for web ...'
+godot --no-window --export "HTML5" dist/web/index.html
+echo 'done.'
+
+echo 'Building for linux ...'
+godot --no-window --export 'Linux/X11' dist/linux/rootedJourney.x86_64
+chmod a+x dist/linux/rootedJourney.x86_64
+echo 'done.'
+
+echo 'Building for Windows ...'
+godot --no-window --export 'Windows Desktop' dist/windows/rootedJourney.exe
+echo 'done.'
+
