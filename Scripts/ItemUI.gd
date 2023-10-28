343fde74491bb7e342cfbb8d41a87e7937836067
Janic Christlieb <janicchristlieb@hotmail.com>
2023-10-28 17:21:14 +0200

    added walls

--- Scripts/ItemUI.gd
+++ Scripts/ItemUI.gd
@@ -5,7 +5,6 @@ func _ready():
 	set_texture(preload("res://Assets/Hand.png"))
 	get_parent().get_parent().item_to_ui.connect(_on_recived_item)
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
