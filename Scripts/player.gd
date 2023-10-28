9e021a7676536d3dc03e2aaf32d6e092a7dfaa82
Janic Christlieb <janicchristlieb@hotmail.com>
2023-10-28 15:49:43 +0200

    added stand up animation and animated idle

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,52 +2,46 @@ extends CharacterBody2D
 
 @export var speed = 700
 var screen_size
+var starting_animation = false
 
 func _ready():
 	# Size of area always size of player collision
 	var player_transform = $CollisionShape2D.transform
 	$Area2D/CollisionShape2D.transform = player_transform
 	screen_size = Vector2(3840,1080)
-	$AnimatedSprite2D.animation = "standing_up"
-	$AnimatedSprite2D.play()
+	$AnimatedSprite2D.play("standing_up")
 
 func _process(delta):
 	var newVelocity = Vector2.ZERO
-	if Input.is_action_pressed("right"):
-		$PointLight2D.position.x = 2000
-		$PointLight2D.position.y = -100
-		newVelocity.x += 1
-	if Input.is_action_pressed("left"):
-		$PointLight2D.position.x = -2200
-		$PointLight2D.position.y = -100
-		newVelocity.x -= 1
-	if not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		$PointLight2D.position.x = 0
-		$PointLight2D.position.y = 300
+	if $AnimatedSprite2D.frame == 10:
+		starting_animation = true	
+	if starting_animation == true:
+		if Input.is_action_pressed("right"):
+			newVelocity.x += 1
+		if Input.is_action_pressed("left"):
+			newVelocity.x -= 1
 
-	if newVelocity.length() > 0:
-		newVelocity = newVelocity.normalized() * speed
-		$AnimatedSprite2D.play()
-	else:
-		$AnimatedSprite2D.stop()
-	position += newVelocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
-	
-	if newVelocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-	else:
-		$AnimatedSprite2D.animation = "idle"
-	
-	if newVelocity.x < 0:
-		$AnimatedSprite2D.flip_h = false
-	else:
-		$AnimatedSprite2D.flip_h = true
+		if newVelocity.length() > 0:
+			newVelocity = newVelocity.normalized() * speed
 		
-	if $AnimatedSprite2D.is_playing() and !$AudioStreamPlayer2D.playing:
-		$AudioStreamPlayer2D.play()
-	elif !$AnimatedSprite2D.is_playing() and $AudioStreamPlayer2D.playing:
-		$AudioStreamPlayer2D.stop()
+		position += newVelocity * delta
+		position = position.clamp(Vector2.ZERO, screen_size)
+		
+		if newVelocity.x != 0:
+			$AnimatedSprite2D.animation = "walk"
+			$AnimatedSprite2D.flip_v = false
+		else:
+			$AnimatedSprite2D.animation = "idle"
+		
+		if newVelocity.x < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+			
+		if $AnimatedSprite2D.is_playing() and !$AudioStreamPlayer2D.playing:
+			$AudioStreamPlayer2D.play()
+		elif !$AnimatedSprite2D.is_playing() and $AudioStreamPlayer2D.playing:
+			$AudioStreamPlayer2D.stop()
 	
 func start(pos):
 	position = pos
