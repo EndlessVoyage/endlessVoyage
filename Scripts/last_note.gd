c7f08a1e46e0af5b365f77f183bb5bbdd0f4e0eb
PyDario <dario@pichowsky.de>
2023-10-29 15:00:38 +0100

    add end screen

---
+++ Scripts/last_note.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var is_ascending = false
+const TRANSITION_SPEED = 1.5 # lower is faster
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$NormalNote.play("default")
+	$Timer.start()
+
+# Once the transition is done, we reset the game
+func _process(delta):
+	if !is_ascending:
+		return
+	
+	var color = $Sprite2D.modulate;
+	var newAlpha = 0.0
+
+	if color.a <= 0:
+		is_ascending = true
+		newAlpha = delta
+	newAlpha = color.a + (delta / TRANSITION_SPEED);
+	var newColor = Color(color, newAlpha)
+	$Sprite2D.modulate = newColor;
+	if $Sprite2D.modulate.a >= 1:
+		get_tree().change_scene_to_file("res://Scenes/death_transition.tscn")
+
+
+func _on_timer_timeout():
+	$NormalNote.visible = false
+	$BurningNote.visible = true
+	$BurningNote.play()
+	is_ascending = true
